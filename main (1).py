990ecae6d5fca72b158e3f75ab1ee2641ed217ae
asper12022bcs147 <144768975+asper12022bcs147@users.noreply.github.com>
2023-09-14 14:04:56 +0530

    Add files via upload

---
+++ main (1).py
@@ -0,0 +1,13 @@
+year = int(input("Enter a year: "))
+
+# Check if it's a leap year using the if-elif-else statement
+if year % 4 == 0:
+    if year % 100 == 0:
+        if year % 400 == 0:
+            print(year, "is a leap year")
+        else:
+            print(year, "is not a leap year")
+    else:
+        print(year, "is a leap year")
+else:
+    print(year, "is not a leap year")
